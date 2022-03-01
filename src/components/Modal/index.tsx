576310e8ef628a669d67fb3c68c46410d790a628
Joao Pedro Noronha <joaopdrons@gmail.com>
2022-03-01 18:16:26 -0300

    solved

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useRef, useState } from 'react';
+import ReactModal from 'react-modal';
+
+const Modal = ({isOpen, children, setIsOpen}: any) => {
+  const [modalStatus, setModalStatus] = useState(isOpen);
+
+  const didMountRef = useRef(false)
+  useEffect(() => {
+    if (didMountRef.current !== isOpen) {
+      setModalStatus(isOpen)
+      didMountRef.current = true
+    } else {
+      didMountRef.current = false
+    }
+  }, [isOpen])
+
+  return (
+    <ReactModal
+      shouldCloseOnOverlayClick={!false}
+      onRequestClose={setIsOpen}
+      isOpen={modalStatus}
+      ariaHideApp={false}
+      style={{
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+          background: '#F0F0F5',
+          color: '#000000',
+          borderRadius: '8px',
+          width: '736px',
+          border: 'none',
+        },
+        overlay: {
+          backgroundColor: '#121214e6',
+        },
+      }}
+    >
+      {children}
+    </ReactModal>
+  );
+};
+
+export default Modal;
